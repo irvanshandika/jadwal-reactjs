85e047c04a66a7a0b445cff27c4d813b1e8ea414
irvanshandika <shandikamuhammadirvan@gmail>
2023-05-26 18:55:00 +0700

    Test commit 4 - JS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import JadwalList from "./components/JadwalList.jsx";
-import AddJadwal from "./components/AddJadwal.jsx";
-import EditJadwal from "./components/EditJadwal.jsx";
+import JadwalList from "./components/JadwalList.js";
+import AddJadwal from "./components/AddJadwal.js";
+import EditJadwal from "./components/EditJadwal.js";
 
 function App() {
   return (
