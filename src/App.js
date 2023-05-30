2ad90a196761d11807a8fb70b531eb494e3bdd9d
irvanshandika <shandikamuhammadirvan@gmail>
2023-05-30 08:52:08 +0700

    Delete Search bar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import JadwalList from "./components/JadwalList.js";
 import AddJadwal from "./components/AddJadwal.js";
 import EditJadwal from "./components/EditJadwal.js";
 import Jumbotron from "./page/Jumbotron.js";
-import Jadwal from "./components/Jadwal.js";
+import Dashboard from "./components/dashboard.js";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
             <Route path="/jadwal" element={<JadwalList />} />
             <Route path="/add" element={<AddJadwal />} />
             <Route path="/edit/:id" element={<EditJadwal />} />
-            <Route path="/dashboard" element={<Jadwal />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </BrowserRouter>
       </div>
