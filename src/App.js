ca6d4fa99f99fa5c7542054d85657176d92b9f0a
irvanshandika <shandikamuhammadirvan@gmail>
2023-05-31 22:06:05 +0700

    Remove Landing Page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home.js";
 import AddJadwal from "./components/AddJadwal.js";
 import EditJadwal from "./components/EditJadwal.js";
-import Jumbotron from "./page/Jumbotron.js";
 import Dashboard from "./components/dashboard.js";
-import Footer from "./components/footer.js";
 
 function App() {
   return (
@@ -13,16 +11,7 @@ function App() {
       <div className="container">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Jumbotron />} />
-            <Route
-              path="/jadwal"
-              element={
-                <>
-                  <Home />
-                  {/* <Footer /> */}
-                </>
-              }
-            />
+            <Route path="/" element={<Home />} />
             <Route path="/add" element={<AddJadwal />} />
             <Route path="/edit/:id" element={<EditJadwal />} />
             <Route path="/dashboard" element={<Dashboard />} />
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -27,7 +27,7 @@ const Dashboard = () => {
           <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
               <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                <Link to={"/jadwal"}>
+                <Link to={"/"}>
                   <button
                     type="button"
                     className="flex items-center justify-center text-white bg-black hover:bg-gray-600 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
